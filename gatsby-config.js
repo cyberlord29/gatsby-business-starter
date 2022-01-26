b001ee6fbd3df0d3c7731f47c4ee60525bce1300
Maneesh <maneesh@ITs-MacBook-Pro.local>
2022-01-26 11:54:53 +0530

    fix seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: '1000Plus Capital',
+    title: 'London Trading Institute',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'The official Business page of London trading Institute',
     },
   plugins: [
     'gatsby-plugin-react-helmet',
