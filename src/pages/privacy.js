691ee00afc03eec4e5a8afc88469f2eef72ea169
Maneesh <maneesh1996@gmail.com>
2022-03-24 03:01:01 +0530

    fix alginemnt

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -29,8 +29,7 @@ const AboutPage = () => {
                 </IntroTitle>
                 <Underline></Underline>
                 <DescriptionContainer style={{fontSize: "14px", margin: "20px 0px", textAlign: "left"}}>
-                This privacy policy has been compiled to better serve those who are concerned with how their ‘Personally identifiable information’ (PII) is being used online.
-                <br/><br/> PII, as used in Privacy and Electronic Communications Regulations, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context.
+                This privacy policy has been compiled to better serve those who are concerned with how their ‘Personally identifiable information’ (PII) is being used online.PII, as used in Privacy and Electronic Communications Regulations, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context.
                 <br/><br/> Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.
                 </DescriptionContainer>
               </div>
